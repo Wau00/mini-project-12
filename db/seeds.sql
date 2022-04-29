129926d14c864a6ec635c709f4e4a86e6b455855
walterunderwood <walonsounderwood@gmail.com>
2022-04-29 14:14:12 -0700

    Working on database

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO movies (movie_name)
+VALUES ("The Shawshank Redemption"),
+       ("The Godfather"),
+       ("The Dark Knight"),
+       ("12 Angry Men ");
+
+INSERT INTO reviews (review, movie_id)
+VALUES ("5-stars", 1),
+       ("4-stars", 2),
+       ("5-stars", 3),
+       ("2-stars", 4);
+       
